6b2daf517f55fbabb621fc783a3cf88de15cb6a0
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2025-02-27 16:50:14 +0700

    Up load Bài tập Session 17_Lesson 02

---
+++ Session 17_Lesson 02.js
@@ -0,0 +1,20 @@
+let bienkhonghople1 = "123number";
+alert(`${bienkhonghople1} đây là biến không hợp lệ`);
+
+let bienkhonghople2= "my var";
+alert(`${bienkhonghople2} đây là biến không hợp lệ`);
+
+let bienkhonghople3= "#name";
+alert(`${bienkhonghople3} đây là biến không hợp lệ`);
+
+let bienhople1 = "myName";
+alert(`${bienhople1} đây là biên hợp lệ`)
+
+let bienhople2 = "_count";
+alert(`${bienhople2} đây là biên hợp lệ`)
+
+let bienhople3 = "number123";
+alert(`${bienhople3} đây là biên hợp lệ`)
+
+
+
